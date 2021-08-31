87baaac2102643613f3d5b764ed189d77d8a4e68
Lucas Santos <lhs.santoss@gmail.com>
2021-08-30 21:17:22 -0300

    Add interaction
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- client/client-web.js
+++ client/client-web.js
@@ -1,5 +1,5 @@
 const { AuthorsClient, BookstoreClient } = require('../proto/bookstore_grpc_web_pb')
-const { Book, Author } = require('../proto/bookstore_pb')
+const { GetBookRequest } = require('../proto/bookstore_pb')
 const { Empty } = require('google-protobuf/google/protobuf/empty_pb')
 
 const port = 8080
@@ -11,25 +11,39 @@ bookstoreClient.listBook(new Empty(), {}, (err, response) => {
     console.log(err)
   } else {
     const books = response.getBooksList().map(book => book.toObject())
-    const booksDiv = document.getElementById('books')
+    const list = document.createElement('ul')
+    document.getElementById('books').appendChild(list)
     books.forEach(book => {
       const item = document.createElement('li')
-      item.innerHTML = `${book.title}`
-      booksDiv.appendChild(item)
+      item.innerHTML = `<a href="#" onclick="loadBook('${book.id}')" type="button">${book.title}</a>`
+      list.appendChild(item)
     })
   }
 })
+
+document.loadBook = (bookId) => {
+  bookstoreClient.getBook(new GetBookRequest().setBookid(bookId), {}, (err, response) => {
+    if (err) {
+      console.log(err)
+    } else {
+      const book = response.getBooks().toObject()
+      const results = document.querySelector('div#result>div')
+      results.innerHTML = `<pre>${JSON.stringify(book, null, 2)}</pre>`
+    }
+  })
+}
 
 authorsClient.listAuthor(new Empty(), {}, (err, response) => {
   if (err) {
     console.log(err)
   } else {
     const authors = response.getAuthorsList().map(author => author.toObject())
-    const authorDiv = document.getElementById('authors')
+    const list = document.createElement('ul')
+    document.getElementById('authors').appendChild(list)
     authors.forEach(author => {
       const item = document.createElement('li')
       item.innerHTML = `${author.name}`
-      authorDiv.appendChild(item)
+      list.appendChild(item)
     })
   }
 })
--- client/client.js
+++ client/client.js
@@ -2,19 +2,30 @@
 const grpc = require('@grpc/grpc-js')
 const protoLoader = require('@grpc/proto-loader')
 const path = require('path')
+const { exit } = require('process')
 
 const protoObject = protoLoader.loadSync(path.resolve(__dirname, '../proto/bookstore.proto'))
 const protoDefinition = grpc.loadPackageDefinition(protoObject)
 
-const bookstoreClient = new protoDefinition.Bookstore('localhost:50051', grpc.credentials.createInsecure())
-const authorClient = new protoDefinition.Authors('localhost:50051', grpc.credentials.createInsecure())
+const port = 8080
+const bookstoreClient = new protoDefinition.Bookstore(`localhost:${port}`, grpc.credentials.createInsecure())
+const authorClient = new protoDefinition.Authors(`localhost:${port}`, grpc.credentials.createInsecure())
 
-bookstoreClient.ListBook({}, (err, books) => {
-  if (err) throw err
-  console.log(books)
+bookstoreClient.ListBook({}, (err, { books }) => {
+  if (err) console.error(err)
+  console.table(books)
 })
 
-authorClient.ListAuthor({}, (err, authors) => {
-  if (err) throw err
-  console.log(authors)
+bookstoreClient.GetBook({ bookId: '936c291012dde742dcf76671e22d324a400d7dac49e93d99b6122853138b291d' }, (err, { books: book }) => {
+  if (err) console.error(err)
+  if (!book) {
+    console.log('Book not found')
+    return
+  }
+  console.table(book)
+})
+
+authorClient.ListAuthor({}, (err, { authors }) => {
+  if (err) console.error(err)
+  console.table(authors)
 })
